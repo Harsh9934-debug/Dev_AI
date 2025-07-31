3049fb59fc8045e08bb632db86d860f716cfdf05
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-07-31 00:18:24 +0000

    Addede the E2B sandbox branch for the file

--- devai/src/inngest/functions.ts
+++ devai/src/inngest/functions.ts
@@ -1,10 +1,18 @@
+import { Sandbox } from "@e2b/code-interpreter";
 import { openai,gemini,createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event,step }) => {
+
+    const sandboxId = await step.run("get-sandbox-id", async ()=>{
+      const sandbox = await Sandbox.create("devai-nextjs-test-2");
+      return sandbox.sandboxId;
+    });
+
      const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert next.js devloper.You write readble ,maintainable code .You write simple next.js and react snippet ",
@@ -15,7 +23,14 @@ export const helloWorld = inngest.createFunction(
       `write the fllowing snippet: ${event.data.value}`,
     );
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url",async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host =  sandbox.getHost(3000);
+      return `https://${host}`;
+    })
+
+
+    return { output, sandboxUrl };
 
   },
 );
